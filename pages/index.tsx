8136e71a7b5fecea1ba2500bb32fff2dec15c785
Matheus Galvão <matheusrigonigalvao@gmail.com>
2023-09-21 11:12:18 +0100

    css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const HomePage: React.FC = () => {
                 Hello, my name is Matheus, I am a Computer Scientist by training and a tech lover by nature.
                 This page was designed to provide information about me in a more straight forward manner.
                 The chat below allows you to talk with a LLM engineered to answer questions and provide information about myself, my career, skills, experience, projects and much more.
-                Hope you find it fun and helpful to know me better!
+                <br />Hope you find it fun and helpful to know me better!
             </h3>
             <div className="homePage__chat">
                 <Chat />
